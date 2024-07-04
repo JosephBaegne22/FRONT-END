3f36842d69ad62c289494b09f5841bbc55379d04
ValentineLefebvre <v.lefebvre.maths@gmail.com>
2024-07-04 15:01:11 +0200

    rebase main_menu

--- screens/MainMenuScreen.js
+++ screens/MainMenuScreen.js
@@ -7,8 +7,8 @@ function MainMenuScreen({ navigation }) {
   const authCtx = useContext(AuthContext);
 
   return (
-    <View>
-		<ImageBackground source={"https://cloudfront-eu-central-1.images.arcpublishing.com/lpguideshopping/UXTKG3TYGJFKZGOOQJGPTXXRSI.jpg"} resizeMode="cover" style={styles.rootContainer}>
+    <View style={styles.rootContainer}>
+		<ImageBackground source={"https://cloudfront-eu-central-1.images.arcpublishing.com/lpguideshopping/UXTKG3TYGJFKZGOOQJGPTXXRSI.jpg"} resizeMode="cover" style={styles.generalContainer}>
 			<View style={styles.menuContainer}>
 				<Text style={styles.title}>Bonjour !</Text>
 				<View
@@ -65,57 +65,61 @@ function MainMenuScreen({ navigation }) {
 export default MainMenuScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 32,
-	height: "100%",
-  },
-  menuContainer: {
-	backgroundColor: "rgba(0, 0, 0, 0.8)",
-    padding: 20,
-	borderRadius: 8,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 8,
-	color: "white",
-  },
-  subTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 8,
-	color: "white",
-  },
-  buttonContainer: {
-    marginTop: 20,
-    marginBottom: 20,
-	justifyContent: "space-between",
-	flexDirection: "row",
-	width: "100%",
-  },
-  mainButtons: {
-	borderRadius: 8,
-	backgroundColor: "#B1B1B1",
-	padding: 8,
-  },
-  leftButtons: {
-	marginRight: 20,
-  },
-  mainButtons: {
-	borderRadius: 8,
-	backgroundColor: "#B1B1B1",
-	padding: 8,
-  },
-  linkButtons: {
-	alignSelf: "flex-end",
-  },
-  linkText: {
-	color: "#888888",
-	textDecorationLine: "underline",
-  }
+	rootContainer: {
+		flex: 1,
+		height: "100%",
+	},
+	generalContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 32,
+		height: "100%",
+	},
+	menuContainer: {
+		backgroundColor: "rgba(0, 0, 0, 0.8)",
+		padding: 20,
+		borderRadius: 8,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: "bold",
+		marginBottom: 8,
+		color: "white",
+	},
+	subTitle: {
+		fontSize: 20,
+		fontWeight: "bold",
+		marginBottom: 8,
+		color: "white",
+	},
+	buttonContainer: {
+		marginTop: 20,
+		marginBottom: 20,
+		justifyContent: "space-between",
+		flexDirection: "row",
+		width: "100%",
+	},
+	mainButtons: {
+		borderRadius: 8,
+		backgroundColor: "#B1B1B1",
+		padding: 8,
+	},
+	leftButtons: {
+		marginRight: 20,
+	},
+	mainButtons: {
+		borderRadius: 8,
+		backgroundColor: "#B1B1B1",
+		padding: 8,
+	},
+	linkButtons: {
+		alignSelf: "flex-end",
+	},
+	linkText: {
+		color: "#888888",
+		textDecorationLine: "underline",
+	}
 });
