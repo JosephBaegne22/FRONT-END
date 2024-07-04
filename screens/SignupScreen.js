ac802b3624b59633e32d1afa4a6db9e0dc3a84ae
djaout lyes <djaoutlyes24@gmail.com>
2024-07-04 17:06:39 +0200

    Refactor SignupScreen with error and success message handling

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,38 +1,58 @@
-import { useContext, useState } from "react";
+import { useState, useEffect } from "react";
 import { Alert } from "react-native";
 
-import AuthContent from "../components/Auth/AuthContent";
+import AuthContent from "../components/auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
-import { AuthContext } from "../store/auth-context";
-import { createUser } from "../util/auth";
+import { createUser, resetPassword } from "../util/auth";
+import { ERROR_MESSAGES, SUCCESS_MESSAGES } from "../constants/messages";
 
-function SignupScreen() {
+function SignupScreen({ route }) {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
 
-  const authCtx = useContext(AuthContext);
+  const isResetPwd = route.params?.resetPwd;
 
-  async function signupHandler({ email, password }) {
+  async function signupHandler({ username, password, secretAnswer }) {
     setIsAuthenticating(true);
     try {
-      const token = await createUser(email, password);
-      authCtx.authenticate(token);
+      let res;
+      if(isResetPwd){
+        res = await resetPassword(username, password, secretAnswer);
+      }else{
+        res = await createUser(username, password, secretAnswer);
+      }
+      const successMessage = SUCCESS_MESSAGES[res.message] || res.message;
+      setMessage(successMessage);
     } catch (error) {
-      Alert.alert(
-        "Échec d'inscription!",
-        "Impossible de vous inscrire, veuillez vérifier vos données et réessayer plus tard."
-      );
+      const errorMessage = ERROR_MESSAGES[error.data.message] || "Une erreur est survenue, veuillez vérifier vos données ou réessayez plus tard !";
+      setError(errorMessage);
+    } finally {
       setIsAuthenticating(false);
     }
   }
 
+  useEffect(() => {
+    if (message) {
+      Alert.alert(message);
+      setMessage("");
+    }
+    if (error) {
+      Alert.alert(error);
+      setError(null);
+    }
+  }, [message, error]);
+
   if (isAuthenticating) {
-    return <LoadingOverlay message="Inscription en cours..." />;
+    return <LoadingOverlay message= {isResetPwd ? "Mise à jour en cours..." : "Inscription en cours..."} />;
   }
 
   return (
     <AuthContent
       onAuthenticate={signupHandler}
-      title="Veuillez vous inscrire !"
+      title={isResetPwd ? "Mise à jour du mot de passe" : "Veuillez vous inscrire !"}
+      isResetPwd={isResetPwd}
+      style={{ paddingTop: isResetPwd ? 16 : 64 }}
     />
   );
 }
