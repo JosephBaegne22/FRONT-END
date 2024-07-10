5214d8e8b551d6a805ca7eb783b3945d52803bc5
ValentineLefebvre <v.lefebvre.maths@gmail.com>
2024-07-10 15:56:07 +0200

    refactor : changing ManualGameEndMenuScreen name to GameEndMenuScreen and taking the auto mode into account

--- screens/GameEndMenuScreen.js
+++ screens/GameEndMenuScreen.js
@@ -7,26 +7,35 @@ import Button from "../components/ui/Button";
 import EndGameTable from "../components/tables/EndGameTable";
 import { Colors, Sizes, FullMenuStyles } from "../constants/styles";
 
-function ManualGameEndMenuScreen({ navigation }) {
+function GameEndMenuScreen({ navigation }) {
   	const authCtx = useContext(AuthContext);
+	// fake data we will need to get in some other way
 	const fakeRaceData = {
 		averageSpeed: 42,
 		highestSpeed: 61,
 		raceTime: 100,
 		hitCount: 5,
-	}
+		finished: false,
+	};
 	const fakeBestData = {
 		averageSpeed: 48,
 		highestSpeed: 67,
 		raceTime: 72,
 		hitCount: 1,
-	}
+	};
+	const raceMode = "auto";
+	// end of fake data
 	return (
 		<View style={FullMenuStyles.rootContainer}>
 			<ImageBackground source={require("../assets/image_fond_menu.jpg")} resizeMode="cover" style={FullMenuStyles.backgroundImage}>
 				<SafeAreaView style={FullMenuStyles.generalContainer}>
 					<View style={FullMenuStyles.menuContainer}>
 						<Text style={[FullMenuStyles.title, styles.title]}>Course terminée</Text>
+						{raceMode === "auto" &&
+							<Text style={[FullMenuStyles.subTitle, styles.subTitle]}>
+								{fakeRaceData.finished ? "Course terminée !" : "Course non terminée. La ligne de guidage a été perdue."}
+							</Text>
+						}
 						<EndGameTable
 							raceData={fakeRaceData}
 							bestData={fakeBestData}
@@ -57,10 +66,13 @@ function ManualGameEndMenuScreen({ navigation }) {
 	);
 }
 
-export default ManualGameEndMenuScreen;
+export default GameEndMenuScreen;
 
 const styles = StyleSheet.create({
 	title:{
 		marginTop: Sizes.M,
-	}
+	},
+	subTitle:{
+		// marginTop: Sizes.M,
+	},
 });
