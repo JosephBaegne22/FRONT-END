00f04f3f2c584d39e18ffbbc876ecee16d3ca916
djaout lyes <djaoutlyes24@gmail.com>
2024-09-06 15:35:46 +0200

    refactor: Remove WebSocket 'message' and 'close' event listeners in websocket.js to simplify connection handling

--- util/websocket.js
+++ util/websocket.js
@@ -11,19 +11,8 @@ socket.addEventListener("open", event => {
   socket.send("Connection established")
 });
 
-// Listen for messages
-socket.addEventListener("message", event => {
-  console.log("Message from server ", event.data)
-});
-
-// Handle connection close
-socket.addEventListener("close", event => {
-  console.log("WebSocket connection closed");
-});
-
 // Handle errors
 socket.addEventListener("error", event => {
-  console.log("WebSocket error", event);
   Alert.alert(
     "Erreur de connexion",
     "Nous avons rencontré un problème de connexion au serveur. Veuillez vérifier votre connexion Internet ou réessayer plus tard."
