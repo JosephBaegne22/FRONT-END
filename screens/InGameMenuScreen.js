bd75a9b280cadd1bc6a6f8218f4ae844f6b2388f
djaout lyes <djaoutlyes24@gmail.com>
2024-09-15 14:17:37 +0200

    feat: Add state management and error handling for ending the game in InGameMenuScreen.js, and update button actions to handle startAt and endAt timestamps

--- screens/InGameMenuScreen.js
+++ screens/InGameMenuScreen.js
@@ -1,14 +1,72 @@
-import { useContext } from "react";
-import { StyleSheet, Text, View, ImageBackground, SafeAreaView } from "react-native";
+import { useContext, useState, useEffect } from "react";
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView, Alert } from "react-native";
 
 import { AuthContext } from "../store/auth-context";
 
 import Button from "../components/ui/Button";
 import { Sizes, FullMenuStyles } from "../constants/styles";
+import { endGame } from "../util/auth";
+import { SUCCESS_MESSAGES, ERROR_MESSAGES } from "../constants/messages";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function InGameMenuScreen({ navigation, route }) {
+  const [IsEndingGame, setIsEndingGame] = useState(false);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
   const authCtx = useContext(AuthContext);
-  const { mode } = route.params;
+  const { mode, seconds, minutes, hours, speed} = route.params;
+
+  const vMax = route.params?.vMax;
+  const vMin = route.params?.vMin;
+  const startAt = route.params?.startAt;
+
+  const duration = seconds + (minutes * 60) + (hours * 3600);
+  
+  const getCurrentDateTime = () => {
+	const now = new Date();
+	const isoString = now.toISOString();
+	return isoString;
+  };
+
+  async function endGameHandler(vMin, vMax, startAt, endAt, duration, mode) {
+    setIsEndingGame(true);
+    try {
+      const data = await endGame(vMin, vMax, startAt, endAt, duration, mode);
+
+      const successMessage =
+        SUCCESS_MESSAGES[data.message] || "Course terminée avec succès !";
+      setMessage(successMessage);
+
+	  navigation.replace("AuthMainMenu");
+    } catch (error) {
+      let errorMessage;
+
+      if (error?.data?.message) {
+        errorMessage = ERROR_MESSAGES[error.data.message] || "Impossible de terminer la course. Veuillez réessayer plus tard!";
+      } else {
+        errorMessage =
+          "Impossible de terminer la course. Veuillez réessayer plus tard!";
+      }
+      setError(errorMessage);
+    } finally {
+      setIsEndingGame(false);
+    }
+  }
+
+  useEffect(() => {
+    if (message) {
+      Alert.alert(message);
+      setMessage("");
+    }
+    if (error) {
+      Alert.alert(error);
+      setError(null);
+    }
+  }, [message, error]);
+
+  if (IsEndingGame) {
+    return <LoadingOverlay message="Fin de la course en cours..." />;
+  }
 
   return (
     <View style={FullMenuStyles.rootContainer}>
@@ -19,13 +77,34 @@ function InGameMenuScreen({ navigation, route }) {
 						<Text style={FullMenuStyles.title}>Jeu en pause</Text>
 						<Button
 							size={styles.button}
-							onPress={() => authCtx.isAuthenticated ? navigation.replace("AuthGame", {mode: mode}) : navigation.replace("Game", {mode: mode})}
+							onPress={() => authCtx.isAuthenticated ? navigation.replace("AuthGame", { 
+								mode: mode,
+								seconds: seconds, 
+								minutes: minutes, 
+								hours: hours, 
+								speed: speed,
+								vMax: vMax,
+								vMin: vMin,
+								startAt: startAt
+							  }) : navigation.replace("Game", { 
+								mode: mode,
+								seconds: seconds, 
+								minutes: minutes, 
+								hours: hours, 
+								speed: speed
+							  })}
 						>
 							Reprendre
 						</Button>
 						<Button
 							size={styles.button}
-							onPress={() => authCtx.isAuthenticated ? navigation.replace("AuthGame", {mode: mode}) : navigation.replace("Game", {mode: mode})}
+							onPress={() => {
+								const startAt = getCurrentDateTime();
+								const params = { mode: mode };
+								if (mode === "manual") {
+									params.startAt = startAt;
+								}
+								authCtx.isAuthenticated ? navigation.replace("AuthGame", params) : navigation.replace("Game", {mode: mode})}}
 						>
 							Recommencer
 						</Button>		
@@ -37,7 +116,22 @@ function InGameMenuScreen({ navigation, route }) {
 						</Button>
 						<Button
 							size={styles.button}
-							onPress={() => authCtx.isAuthenticated ? navigation.replace("AuthMainMenu") : navigation.replace("MainMenu")}
+							onPress={() => {
+								if(authCtx.isAuthenticated){
+									const endAt = getCurrentDateTime();
+									authCtx.setRace({
+										vMin: vMin,
+										vMax: vMax,
+										startAt: startAt,
+										endAt: endAt,
+										duration: duration,
+										mode: mode
+									})
+									endGameHandler(vMin, vMax, startAt, endAt, duration, mode)
+								} else {
+									navigation.replace("MainMenu")
+								}
+								}}
 						>
 							Terminer la course
 						</Button>		
